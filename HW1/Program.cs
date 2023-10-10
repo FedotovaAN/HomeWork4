7b2a8117c26ad1986d82447add4e911fbce283e3
Alla Fedotova <fedotovaan@bk.ru>
2023-10-10 23:38:49 +0300

    added file HW1

---
+++ HW1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int FindDegree(int num, int digr)
+{
+    int degree = 1;
+    for (int i=0; i< digr; i++) 
+    {
+        degree*= num;
+    } 
+    return degree;
+}
+bool ValidateExponent(int digr)
+{
+    if (digr < 0)
+    {
+        Console.Write("Степень не должна быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
+Console.Write("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input digr: ");
+int digr = Convert.ToInt32(Console.ReadLine());
+
+if (ValidateExponent(digr))
+{
+    Console.Write($"число {num} в степени {digr} равно {FindDegree(num, digr)}");
+}
